b735d00cb1bf2a1b83b55aa301f39400ca170648
fortunefuture630 <fortunefuture630@gmail.com>
2025-01-26 11:08:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "opay"
 
-global.banknumber = "8133729715"
+global.banknumber = "9030430910"
 
-global.bankowner = "FADARE"
+global.bankowner = "Evelyn"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
